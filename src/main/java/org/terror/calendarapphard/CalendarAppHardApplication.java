cfeaff4c4fa9a2718010620191df6f3f9cfe3000
kkm4512 <nayoun440@naver.com>
2024-08-19 11:16:56 +0900

    git init

---
+++ src/main/java/org/terror/calendarapphard/CalendarAppHardApplication.java
@@ -0,0 +1,13 @@
+package org.terror.calendarapphard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarAppHardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarAppHardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/terror/calendarapphard/CalendarAppHardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.terror.calendarapphard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarAppHardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
