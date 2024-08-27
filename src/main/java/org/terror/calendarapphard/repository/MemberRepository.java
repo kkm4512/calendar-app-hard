72b3ec1f62a1daf5f6951549a06a6ad224db47cd
kkm4512 <nayoun440@naver.com>
2024-08-27 13:29:54 +0900

    chore. @Repository 추가

---
+++ src/main/java/org/terror/calendarapphard/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package org.terror.calendarapphard.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository {
+}
--- src/main/java/org/terror/calendarapphard/repository/PageableRepository.java
+++ src/main/java/org/terror/calendarapphard/repository/PageableRepository.java
@@ -1,7 +1,9 @@
 package org.terror.calendarapphard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.terror.calendarapphard.entity.Todo;
 
+@Repository
 public interface PageableRepository extends JpaRepository<Todo, Long> {
 }
