3285688e0093f5872ac0f905149b5333361eed9d
kkm4512 <nayoun440@naver.com>
2024-08-27 13:44:39 +0900

    chore. BaseResponseEnum Todo_Not_Found 메서드명 수정 1. USER_NOT_FOUND -> TODO_NOT_FOUND 로 수정

--- src/main/java/org/terror/calendarapphard/enums/BaseResponseEnum.java
+++ src/main/java/org/terror/calendarapphard/enums/BaseResponseEnum.java
@@ -16,12 +16,13 @@ public enum BaseResponseEnum {
     TODO_SAVE_SUCCESS(true, HttpStatus.OK.value(),"일정 저장에 성공 하였습니다"),
     TODO_UPDATE_SUCCESS(true, HttpStatus.OK.value(),"일정 수정에 성공 하였습니다"),
     TODO_DELETE_SUCCESS(true, HttpStatus.OK.value(),"일정 삭제에 성공 하였습니다"),
+    TODO_NOT_FOUND(false, HttpStatus.OK.value(),"요청 하는 일정이 존재하지 않습니다"),
 
     // Comment
     COMMENT_SAVE_SUCCESS(true, HttpStatus.OK.value(),"댓글 저장에 성공 하였습니다"),
-    COMMENT_NOT_FOUND(false,HttpStatus.NOT_FOUND.value(), "요청 하는 댓글이 존재하지 않습니다"),
     COMMENT_UPDATE_SUCCESS(true, HttpStatus.OK.value(),"댓글 수정에 성공 하였습니다"),
-    COMMENT_DELETE_SUCCESS(true, HttpStatus.OK.value(),"댓글 삭제에 성공 하였습니다");
+    COMMENT_DELETE_SUCCESS(true, HttpStatus.OK.value(),"댓글 삭제에 성공 하였습니다"),
+    COMMENT_NOT_FOUND(false,HttpStatus.NOT_FOUND.value(), "요청 하는 댓글이 존재하지 않습니다");
 
 
 
--- src/main/java/org/terror/calendarapphard/util/UtilFind.java
+++ src/main/java/org/terror/calendarapphard/util/UtilFind.java
@@ -3,25 +3,34 @@ package org.terror.calendarapphard.util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.terror.calendarapphard.entity.Comment;
+import org.terror.calendarapphard.entity.Member;
 import org.terror.calendarapphard.entity.Todo;
 import org.terror.calendarapphard.enums.BaseResponseEnum;
 import org.terror.calendarapphard.exceptions.HandleNotFoundException;
 import org.terror.calendarapphard.repository.CommentRepository;
+import org.terror.calendarapphard.repository.MemberRepository;
 import org.terror.calendarapphard.repository.TodoRepository;
 
 @RequiredArgsConstructor
 @Component
 public class UtilFind {
+    private final MemberRepository memberRepository;
     private final TodoRepository todoRepository;
     private final CommentRepository commentRepository;
 
+    //Member 찾는 메서드
+    public Member memberFindById(Long id){
+        return memberRepository.findById(id).orElseThrow(() -> new HandleNotFoundException(BaseResponseEnum.USER_NOT_FOUND));
+    }
+
     // todoId로 todo 찾는 메서드
     public Todo todoFindById(Long id){
-        return todoRepository.findById(id).orElseThrow(() -> new HandleNotFoundException(BaseResponseEnum.USER_NOT_FOUND));
+        return todoRepository.findById(id).orElseThrow(() -> new HandleNotFoundException(BaseResponseEnum.TODO_NOT_FOUND));
     }
 
     //todoId,commentId로 comment 찾는 메서드
     public Comment commentFindById(Long todoId, Long commentId){
         return commentRepository.findByTodoIdAndId(todoId,commentId).orElseThrow(() -> new HandleNotFoundException(BaseResponseEnum.COMMENT_NOT_FOUND));
     }
+
 }
