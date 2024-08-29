81dca5fbac42af8cf26b0018055807c07c9fca1f
kkm4512 <nayoun440@naver.com>
2024-08-29 11:37:36 +0900

    fix. 비밀번호 max 10 -> 20 조정

--- src/main/java/org/terror/calendarapphard/model/memberDto/RequestMemberDto.java
+++ src/main/java/org/terror/calendarapphard/model/memberDto/RequestMemberDto.java
@@ -18,7 +18,7 @@ public class RequestMemberDto {
     @Email(message = "이메일 형식이 아닙니다")
     private String email;
     @NotBlank(message = "비밀번호는 공백 일 수 없습니다")
-    @Size(max = 10, message = "비밀번호는 최대 20글자까지 작성 할 수 있습니다")
+    @Size(max = 20, message = "비밀번호는 최대 20글자까지 작성 할 수 있습니다")
     @Pattern(regexp = "^[a-z0-9!@#$%^&*]+$", message = "비밀번호는 영어 소문자,숫자, 특수기호 (!@#$%^&*) 만 들어올 수 있습니다")
     private String password;
     private String reqAdminKey;
